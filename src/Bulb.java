7ac291ae77971108c606ebed2bfa33b4ef747c3c
moiroudt <thibaut.moiroud@iut-valence.fr>
2014-02-10 10:33:34 +0100

    V1.0 fichiers
    création d'un projet eclipse pour l'exemple de la lampe.

---
+++ src/Bulb.java
@@ -0,0 +1,34 @@
+
+public class Bulb
+{
+	private static final int DEFAULT_POWER = 100;
+	
+	private final int power;
+		
+	private boolean isLit;
+	
+	public Bulb()
+	
+	{
+		this.power = DEFAULT_POWER;
+		this.isLit = false;
+	}
+	
+	public Bulb(int power)
+	
+	{
+		this.power = power;
+		this.isLit = false;
+	}
+	
+	public void switchon()
+	
+	{
+		
+	}
+	
+	public void switchoff()
+	{
+		
+	}
+}
