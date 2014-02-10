dabfe2f51d05e82af64db2cd2b2b6ef69ccae5f9
moiroudt <thibaut.moiroud@iut-valence.fr>
2014-02-10 11:30:26 +0100

    V1.1
    ajout de la doc java

--- src/Bulb.java
+++ src/Bulb.java
@@ -1,12 +1,29 @@
 
+/**
+ * @author moiroudt
+ *
+ */
 public class Bulb
 {
+	/**
+	 * puissance par défaut de la lampe.
+	 */
 	private static final int DEFAULT_POWER = 100;
 	
+	/**
+	 * puissance de la lampe.
+	 */
 	private final int power;
 		
+	/**
+	 * indique si la lampe est allumée ou nonn.
+	 */
 	private boolean isLit;
 	
+	/**
+	 * crée une lampe avec la puissance par défaut.
+	 * la lampe n'est pas allumée.
+	 */
 	public Bulb()
 	
 	{
@@ -14,6 +31,10 @@ public class Bulb
 		this.isLit = false;
 	}
 	
+	/**
+	 * crée une lampe avec une puissance donnée et qui n'est pas allumée
+	 * @param power la puissance donnée
+	 */
 	public Bulb(int power)
 	
 	{
@@ -21,12 +42,18 @@ public class Bulb
 		this.isLit = false;
 	}
 	
+	/**
+	 * action qui allume la nouvelle lampe.
+	 */
 	public void switchon()
 	
 	{
 		
 	}
 	
+	/**
+	 * action qui éteint la nouvelle lampe
+	 */
 	public void switchoff()
 	{
 		
